3d07e64ecd42240bb644e9a8fd50a856d77b56ba
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-05 13:16:38 +0300

    Refactoring

--- src/main/java/com/example/demo.java
+++ src/main/java/com/example/demo.java
@@ -2,14 +2,11 @@ package com.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.*;
@@ -21,14 +18,10 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 @WebServlet(name = "demo", urlPatterns = "/")
 
 public class demo extends HttpServlet {
-
-	private Gson gson = new Gson();
-
 	// new datasource object
 	PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
@@ -48,38 +41,27 @@ public class demo extends HttpServlet {
 		Connection connection = null;
 		try {
 			connection = dataSource.getConnection();
-			// out.println(connection);
-
 			Statement stmt = connection.createStatement();
 			String sql = "select u.user_id, u.first_name, u.last_name, u.birthdate, u.mobile_number, u.mail from users u";
-			// String sql = "select * from users u";
 			ResultSet rs = stmt.executeQuery(sql);
 
 			String result = "";
-
 			List<user> users = new ArrayList<>();
 
-			while(rs.next()){
+			while (rs.next()) {
 				user u = new user(
-					rs.getInt(1), 
-					rs.getString(2), 
-					rs.getString(3),
-					rs.getDate(4), 
-					rs.getString(5),
-					 rs.getString(6)
-				) ;
-				
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getDate(4),
+						rs.getString(5),
+						rs.getString(6));
 				users.add(u);
-				// user u = new user();
 				result = result + u.toString();
-
-				// create json object from user
 			}
 
-				JsonArray a = gson.toJsonTree(users).getAsJsonArray();
-
-				resp.getWriter().write(a.toString());
-
+			JsonArray a = gson.toJsonTree(users).getAsJsonArray();
+			resp.getWriter().write(a.toString());
 
 		} catch (SQLException e) {
 			out.println("my message -> failed to connect to database: "
