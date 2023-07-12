f4c6d1bbe7f49b0bf9fc64afdb8e284729464d2e
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-12 15:26:40 +0300

    save state (modifying code to work with new "user" table but with connection error).

--- src/main/java/com/example/demo.java
+++ src/main/java/com/example/demo.java
@@ -33,41 +33,6 @@ public class demo extends HttpServlet {
 		dataSource.setPassword("2111976");
 	}
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Gson gson = new Gson();
-
-		try (Connection connection = dataSource.getConnection();) {
-			Statement stmt = connection.createStatement();
-			String sql = "select u.user_id, u.first_name, u.last_name, u.birthdate, u.mobile_number, u.mail from users u";
-			ResultSet rs = stmt.executeQuery(sql);
-
-			String result = "";
-			List<user> users = new ArrayList<>();
-
-			while (rs.next()) {
-				user u = new user(
-						rs.getInt(1),
-						rs.getString(2),
-						rs.getString(3),
-						rs.getDate(4).toString(),
-						rs.getString(5),
-						rs.getString(6));
-				users.add(u);
-				result = result + u.toString();
-			}
-
-			JsonArray a = gson.toJsonTree(users).getAsJsonArray();
-			resp.getWriter().write(a.toString());
-
-			stmt.close();
-			rs.close();
-		} catch (SQLException e) {
-			resp.getWriter().write("{\"status\":500} " +"\n" + "Something went wrong while getting data from database");
-		}
-	}
-
-
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -77,12 +42,9 @@ public class demo extends HttpServlet {
 		try (Connection connection = dataSource.getConnection();) {
 			user user = gson.fromJson(new InputStreamReader(request.getInputStream()), user.class);
 
-			CallableStatement stmt = connection.prepareCall("{call add_user(?,?,?,?,?)}");
-			stmt.setString(1, user.getFirst_name());
-			stmt.setString(2, user.getLast_name());
-			stmt.setObject(3, user.getBirthdate());
-			stmt.setString(4, user.getMobile_number());
-			stmt.setString(5, user.getMail());
+			CallableStatement stmt = connection.prepareCall("{call get_user_details(?,?)}");
+			stmt.setString(1, user.getMail());
+			stmt.setString(2, user.getPassword());
 			stmt.executeUpdate();
 
 			stmt.close();
--- src/main/java/com/example/user.java
+++ src/main/java/com/example/user.java
@@ -4,62 +4,109 @@ import java.time.LocalDate;
 
 public class user {
 	int user_id;
-	String first_name; 
-	String last_name; 
-	String birthdate; 
-	String mobile_number; 
+	String first_name;
+	String last_name;
+	String birthdate;
 	String mail;
-	public user(int user_id, String first_name, String last_name, String birthdate, String mobile_number, String mail) {
+	String password;
+	char gender;
+	String mobile;
+	String address;
+
+	public user(int user_id, String first_name, String last_name, String birthdate, String mail, String password,
+			char gender, String mobile, String address) {
 		this.user_id = user_id;
 		this.first_name = first_name;
 		this.last_name = last_name;
 		this.birthdate = birthdate;
-		this.mobile_number = mobile_number;
 		this.mail = mail;
+		this.password = password;
+		this.gender = gender;
+		this.mobile = mobile;
+		this.address = address;
 	}
+
 	public user() {
 	}
+
 	public int getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+
 	public String getFirst_name() {
 		return first_name;
 	}
+
 	public void setFirst_name(String first_name) {
 		this.first_name = first_name;
 	}
+
 	public String getLast_name() {
 		return last_name;
 	}
+
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
 	}
+
 	public LocalDate getBirthdate() {
 		return LocalDate.parse(birthdate);
 	}
+
 	public void setBirthdate(String birthdate) {
 		this.birthdate = birthdate;
 	}
-	public String getMobile_number() {
-		return mobile_number;
-	}
-	public void setMobile_number(String mobile_number) {
-		this.mobile_number = mobile_number;
-	}
+
 	public String getMail() {
 		return mail;
 	}
+
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	@Override
 	public String toString() {
 		return "user [user_id=" + user_id + ", first_name=" + first_name + ", last_name=" + last_name + ", birthdate="
-				+ birthdate + ", mobile_number=" + mobile_number + ", mail=" + mail + "]";
-	} 
+				+ birthdate + ", mail=" + mail + ", password=" + password + ", gender=" + gender + ", mobile=" + mobile
+				+ ", address=" + address + "]";
+	}
 
 
 }
