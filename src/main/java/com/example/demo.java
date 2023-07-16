a391dc2904afbe027e67447a72700bf609c3481b
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-16 15:01:36 +0300

    Call the new db function (get_user_details) using only dopost and do most of the work in db.

--- src/main/java/com/example/demo.java
+++ src/main/java/com/example/demo.java
@@ -4,17 +4,11 @@ import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.UUID;
-
-import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import com.google.gson.Gson;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -24,6 +18,7 @@ public class demo extends HttpServlet {
 
 	private static HikariDataSource dataSource = new HikariDataSource();
 	private final long sessionPeriod = 7200000;
+
 	@Override
 	public void init() {
 		dataSource.setDriverClassName("org.postgresql.Driver");
@@ -33,33 +28,25 @@ public class demo extends HttpServlet {
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		HttpSession session = request.getSession();
-		if (session == null || session.getAttribute("user_id").equals(-1) || session.getAttribute("user_id") == null) {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-			return;
-		}
+		String mail = request.getParameter("mail");
+		String password = request.getParameter("password");
 
-		int user_id = (Integer) session.getAttribute("user_id");
-
-		String userDetails = getUserDetails(user_id);
-
-		response.getWriter().write(userDetails);
-	}
-
-	private String getUserDetails(int user_id) {
 		try (Connection connection = dataSource.getConnection()) {
-			CallableStatement stmt = connection.prepareCall("{call get_user_details(?)}");
-			stmt.setInt(1, user_id);
+			CallableStatement stmt = connection.prepareCall("{call get_user_details(?,?)}");
+			stmt.setString(1, mail);
+			stmt.setString(2, password);
 			ResultSet rs = stmt.executeQuery();
+
 			userDetails userDetails = null;
 			while (rs.next()) {
 				userDetails = new userDetails(
+						rs.getInt("status_code"),
+						rs.getString("message"),
 						rs.getString("first_name"),
 						rs.getString("last_name"),
-						rs.getDate("birthdate").toString(),
+						rs.getDate("birthdate"),
 						rs.getString("mail"),
 						rs.getString("gender"),
 						rs.getString("mobile"),
@@ -67,47 +54,12 @@ public class demo extends HttpServlet {
 						rs.getBigDecimal("balance"));
 			}
 			stmt.close();
+
 			Gson gson = new Gson();
-			return gson.toJson(userDetails);
+			response.getWriter().write(gson.toJson(userDetails));
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-		String mail = request.getParameter("mail");
-		String password = request.getParameter("password");
-		int user_id = authenticateUser(mail, password);
-		String token = UUID.randomUUID().toString();
-
-		HttpSession session = request.getSession();
-		session.setAttribute("user_id", user_id);
-		session.setAttribute("token", token);
-		session.setAttribute("creation_time", new Date().getTime());
-		session.setAttribute("expiry_time", new Date().getTime() + sessionPeriod);
-	}
-
-	private int authenticateUser(String mail, String password) {
-		try (Connection connection = dataSource.getConnection()) {
-			CallableStatement stmt = connection.prepareCall("{call authenticate_user(?,?)}");
-			stmt.setString(1, mail);
-			stmt.setString(2, password);
-			ResultSet rs = stmt.executeQuery();
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnCount = rsmd.getColumnCount();
-			for (int i = 1; i <= columnCount; i++) {
-				System.out.println(rsmd.getColumnName(i));
-			}
-			int user_id = -1;
-			if (rs.next()) {
-				user_id = rs.getInt("result");
-			}
-			stmt.close();
-			return user_id;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
 }
