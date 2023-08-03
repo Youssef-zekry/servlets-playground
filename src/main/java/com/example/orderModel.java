423c390dd52f1c79b8a574f1666b8aa991b04689
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-03 14:48:32 +0300

    Add order model

---
+++ src/main/java/com/example/orderModel.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class orderModel {
+	int product_id;
+	int user_id;
+	int item_count;
+
+	public orderModel(int product_id, int user_id, int item_count) {
+		this.product_id = product_id;
+		this.user_id = user_id;
+		this.item_count = item_count;
+	}
+
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getItem_count() {
+		return item_count;
+	}
+	public void setItem_count(int item_count) {
+		this.item_count = item_count;
+	}
+}
